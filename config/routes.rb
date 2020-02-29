3dfb0dbb0b31ba41169c95e671ed7fa1a2fdb6c2
Mark Richards <mark@lookedafter.com>
2020-02-29 23:18:48 +1100

    lesson 2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :pages, only: [:show]
   namespace :admin do
     resources :pages #/admin/pages
   end
 
+  Page.where.not("slug", nil).all.each do |page|
+    get "/#{page.slug}", controller: "pages", action: "show", id: page.id
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'admin/pages#index'
 end
